e6943693a955e91117cf4079e5925f1c418abbd5
Alexey Taktarov <molefrog@gmail.com>
2023-12-05 14:23:12 +0100

    Library mode.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/library.tsx",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+    },
+  },
+
   plugins: [react({ tsDecorators: true })],
 });
